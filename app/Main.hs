f844fbb6b351a39a4d3dee3f59fdca8eb76a6251
stites <sam@stites.io>
2016-03-27 18:59:28 -0700

    now with ExceptT

--- app/Main.hs
+++ app/Main.hs
@@ -5,24 +5,33 @@ import Web.Scotty
 import Data.Monoid (mconcat)
 import Control.Monad.IO.Class
 import Control.Monad.Trans.Class
-import Control.Monad.Trans.Maybe
+import Control.Monad.Trans.Except
 import Data.Text.Lazy (Text)
-import Data.Maybe (fromMaybe)
+import qualified Data.Text.Lazy as TL
 
 leftErr ::Parsable a => Text -> Either String a
 leftErr k = Left $ "the key: " ++ show k ++ " was missing!"
 
-param' :: Parsable a => Text -> ActionM (Either String a)
-param' k = rescue (Right <$> param k) (const.return $ leftErr k)
+param' :: Parsable a => Text -> ExceptT String ActionM a
+param' k = ExceptT $ rescue (Right <$> param k) (const.return $ leftErr k)
+
+type Reco = (Integer,Integer,Integer,Integer)
+
+tshow = TL.pack . show
 
 main :: IO ()
 main = scotty 3000 $ do
-  get "/:word" $ do
-    beam <- param "word"
-    a <- param' "1"
-    let a' = either (const 0) id a
-    liftIO $ print (a :: Either String Int)
-    liftIO $ print (a' :: Int)
-    html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
+  get "/" $ do
+    reco <- runExceptT $ do
+      a <- param' "1"
+      liftIO $ print a
+      b <- param' "2"
+      c <- param' "3"
+      d <- param' "4"
+      (lift . lift) $ print b
+      return ((a, b, c, d) :: Reco)
+    case reco of
+      Left e -> text (TL.pack e)
+      Right r -> html $ mconcat ["<h1>Scotty, ", tshow r, " me up!</h1>"]
 
 
