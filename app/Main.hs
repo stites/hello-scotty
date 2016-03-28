0fd77609d11fd772362cf6180335fede8cc96b09
stites <sam@stites.io>
2016-03-27 18:41:22 -0700

    use Maybes

--- app/Main.hs
+++ app/Main.hs
@@ -4,6 +4,8 @@ module Main where
 import Web.Scotty
 import Data.Monoid (mconcat)
 import Control.Monad.IO.Class
+import Data.Text.Lazy (Text)
+import Data.Maybe (fromMaybe)
 
 {-
  - newtype ScottyT e m a =
@@ -22,13 +24,18 @@ import Control.Monad.IO.Class
  - class MonadTrans t where
  -   lift :: (Monad m) => m a -> t m a
  -}
+
+param' :: Parsable a => Text -> ActionM (Maybe a)
+param' k = rescue (Just <$> param k) (const (return Nothing))
+
 main :: IO ()
 main = scotty 3000 $ do
   -- get :: RoutePattern -> ActionM () -> ScottyM ()
   get "/:word" $ do
-    beam <- param "word"
-    -- notice that the IO is placed in ActionM:
-    liftIO (putStrLn "hello")
+    beam' <- param' "word"
+    let beam = fromMaybe "" beam'
+    i <- param' "num"
+    liftIO $ print (i :: Maybe Integer)
     html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
 
 
