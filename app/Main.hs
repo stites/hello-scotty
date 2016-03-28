c9a0bcdc85552dcc4265e86918596e900c6f9508
stites <sam@stites.io>
2016-03-27 18:45:57 -0700

    more putStrLning

--- app/Main.hs
+++ app/Main.hs
@@ -4,6 +4,8 @@ module Main where
 import Web.Scotty
 import Data.Monoid (mconcat)
 import Control.Monad.IO.Class
+import Control.Monad.Trans.Class
+import Control.Monad.Trans.Maybe
 import Data.Text.Lazy (Text)
 import Data.Maybe (fromMaybe)
 
@@ -25,17 +27,25 @@ import Data.Maybe (fromMaybe)
  -   lift :: (Monad m) => m a -> t m a
  -}
 
-param' :: Parsable a => Text -> ActionM (Maybe a)
-param' k = rescue (Just <$> param k) (const (return Nothing))
+param' :: Parsable a => Text -> MaybeT ActionM a
+param' k = MaybeT $ rescue (Just <$> param k) (const (return Nothing))
+
+type Reco = (Integer, Integer, Integer, Integer)
 
 main :: IO ()
 main = scotty 3000 $ do
   -- get :: RoutePattern -> ActionM () -> ScottyM ()
   get "/:word" $ do
-    beam' <- param' "word"
-    let beam = fromMaybe "" beam'
-    i <- param' "num"
-    liftIO $ print (i :: Maybe Integer)
+    beam <- param "word"
+    -- reco :: Maybe Reco
+    reco <- runMaybeT $ do
+      a <- param' "1"
+      b <- param' "2"
+      c <- param' "3"
+      d <- param' "4"
+      (lift.lift) $ print b
+      return ((a,b,c,d) :: Reco)
+    liftIO $ print reco
     html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
 
 
